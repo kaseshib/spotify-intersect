df919e3c57ae284ee6586ac41be496595ac3cff7
Kasey Shibayama <kaseshib@gmail.com>
2020-10-03 18:35:06 -0700

    messy, developed login flow

---
+++ intersect.py
@@ -0,0 +1,97 @@
+import spotipy
+from spotipy.oauth2 import SpotifyOAuth
+
+spotify = spotipy.Spotify(auth_manager=SpotifyOAuth(
+    scope="user-library-read playlist-modify-private"))
+
+
+class Song():
+    def __init__(self, song, artist, id):
+        self.name = song
+        self.artist = artist
+        self.id = id
+
+    def __eq__(self, other):
+        return self.id == other.id
+        # return self._name == other.getName() and \
+        #     self._artist == other.getArtist()
+
+    def __hash__(self):
+        return hash(self.id)
+
+
+def allLiked(user):
+    user_set = set()
+    index_at = 0
+    results = user.current_user_saved_tracks(limit=50, offset=index_at)
+
+    while results['items']:
+        for index, item in enumerate(results['items']):
+            track = item['track']
+            user_set.add(
+                Song(track['name'], track['artists'][0]['name'], track['id']))
+            # if index % 10 == 0:
+            #     print(index_at + index + 1, ' - ', track['name'], ' - ',
+            #           track['artists'][0]['name'], ' - ', track['id'])
+        index_at += 50
+        print(index_at)
+        results = user.current_user_saved_tracks(limit=50, offset=index_at)
+    print("leaving function...")
+    return user_set
+
+
+def setIntersect(user1, user2):
+    print('ahahh')
+    set1 = allLiked(user1)
+    print('-- done with user1 --')
+    set2 = allLiked(user2)
+    print('-- done with user2 --')
+    return set1.intersection(set2)
+
+
+def savePlaylist(user, set):
+    user_id = user.me()['id']
+    playlist = user.user_playlist_create(
+        user=user_id, name="Intersect!", public=False)
+    song_ids = [i.id for i in set]
+
+    while len(song_ids) > 100:
+        user.user_playlist_add_tracks(user_id, playlist['id'], song_ids[:100])
+        song_ids[:] = song_ids[100:]
+
+    user.user_playlist_add_tracks(user_id, playlist['id'], song_ids)
+    print("*** successfully saved playlist ***")
+
+# index_at = 0
+# user1_set = set()
+# results = spotify.current_user_saved_tracks(limit=50, offset=index_at)
+
+# # print(spotify.current_user_saved_tracks(limit=50, offset=900))
+
+# while results['items']:
+#     for index, item in enumerate(results['items']):
+#         track = item['track']
+
+#         user1_set.add(
+#             Song(track['name'], track['artists'][0]['name'], track['id']))
+
+#         print(index_at + index + 1, ' - ', track['name'], ' - ',
+#               track['artists'][0]['name'], ' - ', track['id'])
+#     index_at += 120
+#     results = spotify.current_user_saved_tracks(limit=50, offset=index_at)
+
+# user2_set = set()
+# user2_set.add(Song('song name', 'artist!', "19Ov4l8mtvCT1iEUKks4aM"))
+# user2_set.add(Song('Two-Headed Boy', "Neutral Milk Hotel", 'codesasdfjkl'))
+# user2_set.add(Song('dreamer', 'sas', '2sNhYJ2ggd9BadlTXU3Mah'))
+
+# both = user1_set.intersection(user2_set)
+
+# user1 = spotify.me()['id']
+# test_playlist = spotify.user_playlist_create(
+#     user=user1, name="test", public=False)
+
+# ids = [i.id for i in both]
+# print(ids)
+# spotify.user_playlist_add_tracks(user1, test_playlist['id'], ids)
+# # print(test_playlist)
--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request, session, redirect
+from flask import Flask, render_template, request, session, redirect, url_for
 from flask_session import Session
 import spotipy
 from spotipy.oauth2 import SpotifyOAuth
@@ -6,6 +6,7 @@ from spotipy import oauth2
 import uuid
 import os
 from pprint import pprint
+import intersect
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = os.urandom(64)
@@ -19,114 +20,218 @@ if not os.path.exists(caches_folder):
 
 
 def session_cache_path(user):
-    print(f"user {user} cache:", caches_folder + session.get(f'uuid_{user}'))
-
     return caches_folder + session.get(f'uuid_{user}')
 
 
 # spotify = spotipy.Spotify(auth_manager=SpotifyOAuth(
 #     scope="user-library-read playlist-modify-private"))
 
+def clear_caches():
+    try:
+        if session.get('uuid_1') and os.path.exists(session_cache_path(1)):
+            print('yeaaaa')
+            os.remove(session_cache_path(1))
+        if session.get('uuid_2') and os.path.exists(session_cache_path(2)):
+            print('ookmkkkk')
+            os.remove(session_cache_path(2))
+        session.clear()
+    except OSError as e:
+        print("Error: %s - %s." % (e.filename, e.strerror))
+
 
 @ app.route('/')
 def index():
-    print("---------- NEW REFRESH ----------")
-    if not session.get('uuid_1'):  # or session.get('uuid_2'):
-        # Step 1. There is no user 1, give random ID
 
-        session['uuid_1'] = str(uuid.uuid4())
-        # session['uuid_2'] = str(uuid.uuid4())
-    if not session.get('uuid_2'):
-        # There is no user 2, give random ID
-        session['uuid_2'] = str(uuid.uuid4())
+    clear_caches()
+    session['uuid_1'] = str(uuid.uuid4())
 
     auth_manager_1 = spotipy.oauth2.SpotifyOAuth(
         scope='user-library-read playlist-modify-private', cache_path=session_cache_path(1), show_dialog=True)
 
-    auth_manager_2 = spotipy.oauth2.SpotifyOAuth(
-        scope='user-library-read playlist-modify-private', cache_path=session_cache_path(2), show_dialog=True)
+    auth_url_1 = auth_manager_1.get_authorize_url()
 
-    if request.args.get("code"):
-        # Step 3. Being redirected from Spotify auth page
-        print('redirected here')
-        if not auth_manager_1.get_cached_token():
-            auth_manager_1.get_access_token(request.args.get("code"))
-        else:
-            auth_manager_2.get_access_token(request.args.get("code"))
-        return redirect('/')
+    return render_template('signin.html', url=auth_url_1, first=True)
 
-    auth_url_1 = auth_url_2 = spotify_1 = spotify_2 = None
+    # return render_template('user1.html')
 
-    # print("auth_1 token:", auth_manager_1.get_cached_token())
+    print("---------- NEW REFRESH ----------")
+
+    # implement
+    ''' main:
+    if no user 1:
+        clear_caches()
+        create_session_id(1)
+    elif no user 2:
+        create_session_id(2)
+    else:
+        display intersect playlist with option to save
+
+    if redirected from auth page:
+        get access token
+        return same template
+
+    display sign in link for correct user
+
+
+
+'''
+    #######################
+
+    # if not session.get('uuid_1'):  # or session.get('uuid_2'):
+    #     # Step 1. There is no user 1, give random ID
+    #     # clear_caches()
+    #     session['uuid_1'] = str(uuid.uuid4())
+    # if not session.get('uuid_2'):
+    #     session['uuid_2'] = str(uuid.uuid4())
+    # # else:
+    #     # return render_template('playlist.html')
+
+    # auth_manager_1 = spotipy.oauth2.SpotifyOAuth(
+    #     scope='user-library-read playlist-modify-private', cache_path=session_cache_path(1), show_dialog=True)
+
+    # auth_manager_2 = spotipy.oauth2.SpotifyOAuth(
+    #     scope='user-library-read playlist-modify-private', cache_path=session_cache_path(2), show_dialog=True)
+
+    # if request.args.get("code"):
+    #     # Step 3. Being redirected from Spotify auth page
+    #     print('redirected here')
+    #     if not auth_manager_1.get_cached_token():
+    #         auth_manager_1.get_access_token(request.args.get("code"))
+    #     else:
+    #         auth_manager_2.get_access_token(request.args.get("code"))
+    #         return redirect(url_for('playlist'))
+    #     return redirect('/')
+
+    # auth_url_1 = auth_url_2 = spotify_1 = spotify_2 = None
+
+    # # Step 2. Display sign in link when no token
     # if not auth_manager_1.get_cached_token():
-    #     print("**** BOOOO ****")
-    #     # Step 2. Display sign in link when no token
     #     auth_url_1 = auth_manager_1.get_authorize_url()
-    #     # return f'<h2><a href="{auth_url_1}">Sign in</a></h2>'
-    #     return render_template('index.html', user1=None, user2=None, auth_1=auth_url_1, auth_2=auth_url_2)
-
-    # Step 2. Display sign in link when no token
-    if not auth_manager_1.get_cached_token():
-        auth_url_1 = auth_manager_1.get_authorize_url()
-    else:
-        spotify_1 = spotipy.Spotify(auth_manager=auth_manager_1)
-
-    if not auth_manager_2.get_cached_token():
-        auth_url_2 = auth_manager_2.get_authorize_url()
-    else:
-        spotify_2 = spotipy.Spotify(auth_manager=auth_manager_2)
+    # else:
+    #     spotify_1 = spotipy.Spotify(auth_manager=auth_manager_1)
 
     # if not auth_manager_2.get_cached_token():
-    #     print("#### WOOHOO ####")
-    #     # Step 2. Display sign in link when no token
     #     auth_url_2 = auth_manager_2.get_authorize_url()
-    #     # return f'<h2><a href="{auth_url_2}">Sign in</a></h2>'
-    #     return render_template('index.html', user1=spotify_1.me(), user2=None, auth_1=auth_url_1, auth_2=auth_url_2)
+    # else:
+    #     spotify_2 = spotipy.Spotify(auth_manager=auth_manager_2)
 
-    # Step 4. Signed in, display data
-    # spotify_1 = spotipy.Spotify(auth_manager=auth_manager_1)
-    # spotify_2 = spotipy.Spotify(auth_manager=auth_manager_2)
-    user1 = spotify_1.me() if spotify_1 else None
-    user2 = spotify_2.me() if spotify_2 else None
-    return render_template('index.html', user1=user1, user2=user2, auth_1=auth_url_1, auth_2=auth_url_2)
+    # # if not auth_manager_2.get_cached_token():
+    # #     print("#### WOOHOO ####")
+    # #     # Step 2. Display sign in link when no token
+    # #     auth_url_2 = auth_manager_2.get_authorize_url()
+    # #     # return f'<h2><a href="{auth_url_2}">Sign in</a></h2>'
+    # #     return render_template('index.html', user1=spotify_1.me(), user2=None, auth_1=auth_url_1, auth_2=auth_url_2)
+
+    # # Step 4. Signed in, display data
+    # user1 = spotify_1.me() if spotify_1 else None
+    # user2 = spotify_2.me() if spotify_2 else None
+
+    # # user = user1 if not user1 else user2
+    # # url = auth_url_1 if user == user1 else auth_url_2
+    # user = url = None
+    # first = False
+
+    # if not user1:
+    #     user = user1
+    #     first = True
+    #     url = auth_url_1
+    # elif not user2:
+    #     user = user2
+    #     first = False
+    #     url = auth_url_2
+    # else:
+    #     return redirect(url_for('playlist'))
+
+    #     both = intersect.setIntersect(spotify_1, spotify_2)
+    #     return render_template('playlist.html', songs=both)
+
+    # return render_template('signin.html', user=user, url=url, first=first)
+
+    # return render_template('index.html', user1=user1, user2=user2,  auth_1=auth_url_1, auth_2=auth_url_2)
     # return render_template('index.html', user1=spotify_1.me(), user2=spotify_2.me(), auth_1=auth_url_1, auth_2=auth_url_2)
 
 
-@app.route('/sign_out/<user>')
-def sign_out(user):
-    other_user = "1" if user == "2" else "2"
-    try:
-        # Remove the CACHE file (.cache-test) so that a new user can authorize.
-        print("signing out...")
-        os.remove(session_cache_path(user))
-        if not os.path.exists(session_cache_path(other_user)):
-            print('-- other user --', other_user)
-            print('-- user -- ', user)
-            session.clear()
-    except OSError as e:
-        print("Error: %s - %s." % (e.filename, e.strerror))
-    return redirect('/')
+@ app.route('/callback/')
+def callback():
+    auth_manager_1 = spotipy.oauth2.SpotifyOAuth(
+        scope='user-library-read playlist-modify-private', cache_path=session_cache_path(1), show_dialog=True)
+
+    print('got here')
+    if not auth_manager_1.get_cached_token():
+        auth_manager_1.get_access_token(request.args.get("code"))
+        session['uuid_2'] = str(uuid.uuid4())
+
+        print('in here too')
+
+        auth_manager_2 = spotipy.oauth2.SpotifyOAuth(
+            scope='user-library-read playlist-modify-private', cache_path=session_cache_path(2), show_dialog=True)
+        auth_url_2 = auth_manager_2.get_authorize_url()
+
+        return render_template('signin.html', url=auth_url_2, first=False)
+    else:
+        auth_manager_2 = spotipy.oauth2.SpotifyOAuth(
+            scope='user-library-read playlist-modify-private', cache_path=session_cache_path(2), show_dialog=True)
+
+        auth_manager_2.get_access_token(request.args.get("code"))
+        return redirect(url_for('playlist'))
 
 
-@app.route('/playlists/<user>')
-def playlists(user):
-    auth_manager = spotipy.oauth2.SpotifyOAuth(
-        cache_path=session_cache_path(user))
-    if not auth_manager.get_cached_token():
-        return redirect('/')
+# @ app.route('/sign_out/<user>')
 
-    spotify = spotipy.Spotify(auth_manager=auth_manager)
-    return spotify.current_user_playlists()
+    # other_user = "1" if user == "2" else "2"
+    # try:
+    #     # Remove the CACHE file (.cache-test) so that a new user can authorize.
+    #     print("signing out...")
+    #     os.remove(session_cache_path(user))
+
+    #     if not os.path.exists(session_cache_path(other_user)):
+    #         print('-- other user --', other_user)
+    #         print('-- user -- ', user)
+    #         session.clear()
+    # except OSError as e:
+    #     print("Error: %s - %s." % (e.filename, e.strerror))
+    # return redirect('/')
 
 
-@app.route('/current_user/<user>')
-def current_user(user):
-    auth_manager = spotipy.oauth2.SpotifyOAuth(
-        cache_path=session_cache_path(user))
-    if not auth_manager.get_cached_token():
-        return redirect('/')
-    spotify = spotipy.Spotify(auth_manager=auth_manager)
-    return spotify.current_user()
+# @ app.route('/playlists/<user>')
+# def playlists(user):
+#     auth_manager = spotipy.oauth2.SpotifyOAuth(
+#         cache_path=session_cache_path(user))
+#     if not auth_manager.get_cached_token():
+#         return redirect('/')
+
+#     spotify = spotipy.Spotify(auth_manager=auth_manager)
+#     return spotify.current_user_playlists()
+
+
+# @ app.route('/current_user/<user>')
+# def current_user(user):
+#     auth_manager = spotipy.oauth2.SpotifyOAuth(
+#         cache_path=session_cache_path(user))
+#     if not auth_manager.get_cached_token():
+#         return redirect('/')
+#     spotify = spotipy.Spotify(auth_manager=auth_manager)
+#     return spotify.current_user()
+
+@ app.route('/test')
+def test():
+    return 'hello there'
+
+
+@ app.route('/playlist')
+def playlist():
+    auth1 = spotipy.oauth2.SpotifyOAuth(
+        cache_path=session_cache_path(1))
+    auth2 = spotipy.oauth2.SpotifyOAuth(
+        cache_path=session_cache_path(2))
+
+    user1 = spotipy.Spotify(auth_manager=auth1)
+    user2 = spotipy.Spotify(auth_manager=auth2)
+
+    both = intersect.setIntersect(user1, user2)
+    # intersect.savePlaylist(user1, both)
+
+    return render_template('playlist.html', songs=both)
 
 
 if __name__ == "__main__":
